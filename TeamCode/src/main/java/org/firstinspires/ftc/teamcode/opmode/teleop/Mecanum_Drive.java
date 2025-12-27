8fcd0d2627e557350be8a5a62e536e3996624db8
Nitikan <kangood23626@gmail.com>
2025-12-27 21:20:08 +0700

    ii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Mecanum_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Mecanum_Drive.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawOnlyCurrent;
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -9,63 +14,51 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.HeadingInterpolator;
 import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 @Configurable
+@Disabled
 @TeleOp
-public class Mecanum_Drive extends OpMode {
+public abstract class Mecanum_Drive extends OpMode {
     private Follower follower;
     public static Pose startingPose; //See ExampleAuto to understand how to use this
     private boolean automatedDrive;
     private Supplier<PathChain> pathChain;
     private TelemetryManager telemetryM;
     private boolean slowMode = false;
-    private double slowModeMultiplier = 0.5;
-    private double A, B, C, D, XGame1, YGame1, XGame2, YGame2, SpeedMove, SpeedLimit, SpeedTurn, drs, dls;
-
+    public double SpeedLimit = 1;
+    public double A, B, C, D, XGame1, YGame1, XGame2, YGame2, SpeedMove, SpeedTurn, drs, dls;
 
     @Override
-    public void init() {
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+    public void init_loop(){
+        telemetryM.debug("This will print your robot's position to telemetry while "
+                + "allowing robot control through a basic mecanum drive on gamepad 1.");
+        telemetryM.update(telemetry);
         follower.update();
-        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
-
-        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
-                .addPath(new Path(new BezierLine(follower::getPose, new Pose(45, 98))))
-                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
-                .build();
+        drawOnlyCurrent();
     }
 
-    @Override
-    public void start() {
-    }
-
-
-    @Override
-    public void loop() {
-
-    }
-
-    public void move() {
-
+    public double[] move() {
+        this.SpeedLimit = SpeedLimit;
         XGame1 = gamepad1.left_stick_x * -1;
         YGame1 = gamepad1.left_stick_y * -1;
         XGame2 = gamepad1.right_stick_x * -1;
         YGame2 = gamepad1.right_stick_y * -1;
 
-
-            SpeedMove = Math.sqrt(Math.pow(XGame1, 2) + Math.pow(YGame1, 2)) * SpeedLimit;
-            SpeedTurn = Math.sqrt(Math.pow(XGame2, 2) + Math.pow(YGame2, 2)) * (SpeedLimit - 0.1);
-            drs = Math.sin((Math.atan2(YGame1, XGame1) / Math.PI * 180 - 45) / 180 * Math.PI);
-            dls = Math.cos((Math.atan2(YGame1, XGame1) / Math.PI * 180 - 45) / 180 * Math.PI);
-            if (YGame1 < 0) {
+        SpeedMove = Math.sqrt(Math.pow(XGame1, 2) + Math.pow(YGame1, 2)) * SpeedLimit;
+        SpeedTurn = Math.sqrt(Math.pow(XGame2, 2) + Math.pow(YGame2, 2)) * (SpeedLimit - 0.1);
+        drs = Math.sin((Math.atan2(YGame1, XGame1) / Math.PI * 180 - 45) / 180 * Math.PI);
+        dls = Math.cos((Math.atan2(YGame1, XGame1) / Math.PI * 180 - 45) / 180 * Math.PI);
+        if (YGame1 < 0) {
                 if (dls >= drs) {
                     dls = -(dls / drs);
                     drs = -1;
@@ -73,7 +66,7 @@ public class Mecanum_Drive extends OpMode {
                     drs = -(drs / dls);
                     dls = -1;
                 }
-            } else {
+        } else {
                 if (drs <= dls) {
                     drs = drs / dls;
                     dls = 1;
@@ -92,5 +85,6 @@ public class Mecanum_Drive extends OpMode {
         C += Math.sin((Math.atan2(YGame2, XGame2) / Math.PI * 180 - 45) / 180 * Math.PI) * SpeedTurn;
         D += Math.cos((Math.atan2(YGame2, XGame2) / Math.PI * 180 - 45) / 180 * Math.PI) * SpeedTurn;
 
-
+        draw();
+        return new double[] {A,B,C,D};
 }}
